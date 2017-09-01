23b32d8a05e91fc79fa5e081bca312637c2f0781
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2017-09-01 17:32:44 +0300

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -12,6 +12,18 @@ let io = socketIO(server);
 io.on('connection', socket => {
     console.log(`New user connected`);
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createAt: new Date().getTime()
+    });
+    
     socket.on('createMessage', message => {
         console.log(`CreateMessage `, message);
         io.emit('newMessage', {
@@ -19,6 +31,12 @@ io.on('connection', socket => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => console.log(`User was disconnected`));
