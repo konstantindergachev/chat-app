18409d0b0ec1dfe2a88b75179c0640e39b87bfb4
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2019-03-06 15:19:53 +0200

    Remove playground dir

--- playground/time.js
+++
@@ -1,9 +0,0 @@
-let moment = require('moment');
-
-let someTimestamp = moment().valueOf();
-console.log(someTimestamp);
-
-let createdAt = 1234;
-let date = moment(createdAt);
-
-console.log(date.format('h:mm a'));
