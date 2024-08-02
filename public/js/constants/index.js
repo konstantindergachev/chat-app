f6d33d2be76b349d68b73a13c6cf2b19e2273989
Kostyantyn Dergachov <dergachevkonstantin@gmail.com>
2024-08-02 13:11:10 +0000

    Took string values from constants

---
+++ public/js/constants/index.js
@@ -0,0 +1,21 @@
+export const NAME_PLACEHOLDER = "Введите имя";
+export const ROOM_PLACEHOLDER = "Введите имя комнаты";
+export const NAME_ERROR = "Введите ваше имя";
+export const ROOM_ERROR = "Введите вашу комнату";
+
+export const USER_INPUT = "username";
+export const ROOM_INPUT = "room";
+export const USER_JOINED = "Chat is started";
+
+export const USER_COLOR_FIRST = "#a2836e";
+export const USER_COLOR_SECOND = "#674d3c";
+
+export const ADMIN_ROLE = "Админ:";
+export const ADMIN_VALUE = "admin";
+
+export const GEOLOCATION_LOADING = "Отправка местоположения...";
+export const GEOLOCATION_RULE = "Отправь местоположение";
+export const GEOLOCATION_ERROR = "Геолокация не поддерживается вашим браузером";
+export const SERVER_GEOLOCATION_ERROR =
+  "Нет возможности отправить ваше местоположение";
+export const MAP = "Карта";
