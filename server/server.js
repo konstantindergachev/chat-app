295728167193b494488f82c3745d3f5920a4a076
Kostyantyn Dergachov <dergachevkonstantin@gmail.com>
2024-08-05 09:39:30 +0000

    Put the chat logic to a separate file.

--- server/server.js
+++ server/server.js
@@ -2,101 +2,14 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const socketIO = require("socket.io");
-const { isRealString } = require("./utils/validation");
-const { Users } = require("./utils/users");
-const { generateMessage, generateLocationMessage } = require("./utils/message");
-const { generateUniqueColor } = require("./utils/color");
-const {
-  CHAT_JOIN_ERROR,
-  ADMIN_ROLE,
-  ADMIN_VALUE,
-  USER_CAME_TO_THE_CHAT,
-  USER_DISCONNECTED,
-  USER_LEAVE_THE_CHAT,
-  SERVER_STARTED,
-} = require("./constants");
+const { SERVER_STARTED } = require("./constants");
+const { chatHandler } = require("./utils/chat");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
-const users = new Users();
 
-io.on("connection", (socket) => {
-  socket.on("join", (params, callback) => {
-    console.log(`${params.name} joined to chat.`);
-    if (!isRealString(params.name) || !isRealString(params.room))
-      return callback(CHAT_JOIN_ERROR);
-
-    const userColors = generateUniqueColor();
-    socket.join(params.room);
-    users.removeUser(socket.id);
-    users.addUser(socket.id, params.name, params.room, userColors);
-
-    io.to(params.room).emit(
-      "updateUserList",
-      users.getUserList(params.room),
-      params.name
-    );
-    socket.emit("newMessage", generateMessage(ADMIN_ROLE, ADMIN_VALUE));
-    socket.emit("assignColors", userColors);
-    socket.broadcast
-      .to(params.room)
-      .emit(
-        "newMessage",
-        generateMessage(ADMIN_ROLE, `${params.name} ${USER_CAME_TO_THE_CHAT}`)
-      );
-
-    callback();
-  });
-
-  socket.on("createMessage", (message, callback) => {
-    const user = users.getUser(socket.id);
-    const clientsCount = socket.client.server.eio.clientsCount;
-    if (user && isRealString(message.text))
-      io.to(user.room).emit(
-        "newMessage",
-        generateMessage(
-          `${user.name}:`,
-          message.text,
-          clientsCount,
-          user.colors
-        )
-      );
-
-    callback();
-  });
-
-  socket.on("createLocationMessage", (coords) => {
-    const user = users.getUser(socket.id);
-
-    if (user)
-      io.to(user.room).emit(
-        "newLocationMessage",
-        generateLocationMessage(
-          user.name,
-          coords.latitude,
-          coords.longitude,
-          user.colors
-        )
-      );
-  });
-
-  socket.on("disconnect", () => {
-    const user = users.removeUser(socket.id);
-    console.log(`${user.name} ${USER_DISCONNECTED}`);
-    if (user) {
-      io.to(user.room).emit(
-        "updateUserList",
-        users.getUserList(user.room),
-        user.name
-      );
-      io.to(user.room).emit(
-        "newMessage",
-        generateMessage(ADMIN_ROLE, `${user.name} ${USER_LEAVE_THE_CHAT}`)
-      );
-    }
-  });
-});
+chatHandler(io);
 
 const publicPath = path.join(__dirname, "../public");
 app.use(express.static(publicPath));
---
+++ server/utils/chat.js
@@ -0,0 +1,94 @@
+const { Users } = require("./users");
+const { isRealString } = require("./validation");
+const { generateMessage, generateLocationMessage } = require("./message");
+const { generateUniqueColor } = require("./color");
+const {
+  CHAT_JOIN_ERROR,
+  ADMIN_ROLE,
+  ADMIN_VALUE,
+  USER_CAME_TO_THE_CHAT,
+  USER_DISCONNECTED,
+  USER_LEAVE_THE_CHAT,
+} = require("../constants");
+
+const users = new Users();
+function chatHandler(io) {
+  io.on("connection", (socket) => {
+    socket.on("join", (params, callback) => {
+      console.log(`${params.name} joined to chat.`);
+      if (!isRealString(params.name) || !isRealString(params.room))
+        return callback(CHAT_JOIN_ERROR);
+
+      const userColors = generateUniqueColor();
+      socket.join(params.room);
+      users.removeUser(socket.id);
+      users.addUser(socket.id, params.name, params.room, userColors);
+
+      io.to(params.room).emit(
+        "updateUserList",
+        users.getUserList(params.room),
+        params.name
+      );
+      socket.emit("newMessage", generateMessage(ADMIN_ROLE, ADMIN_VALUE));
+      socket.emit("assignColors", userColors);
+      socket.broadcast
+        .to(params.room)
+        .emit(
+          "newMessage",
+          generateMessage(ADMIN_ROLE, `${params.name} ${USER_CAME_TO_THE_CHAT}`)
+        );
+
+      callback();
+    });
+
+    socket.on("createMessage", (message, callback) => {
+      const user = users.getUser(socket.id);
+      const clientsCount = socket.client.server.eio.clientsCount;
+      if (user && isRealString(message.text))
+        io.to(user.room).emit(
+          "newMessage",
+          generateMessage(
+            `${user.name}:`,
+            message.text,
+            clientsCount,
+            user.colors
+          )
+        );
+
+      callback();
+    });
+
+    socket.on("createLocationMessage", (coords) => {
+      const user = users.getUser(socket.id);
+
+      if (user)
+        io.to(user.room).emit(
+          "newLocationMessage",
+          generateLocationMessage(
+            user.name,
+            coords.latitude,
+            coords.longitude,
+            user.colors
+          )
+        );
+    });
+
+    socket.on("disconnect", () => {
+      const user = users.removeUser(socket.id);
+      console.log(`${user.name} ${USER_DISCONNECTED}`);
+      if (user) {
+        io.to(user.room).emit(
+          "updateUserList",
+          users.getUserList(user.room),
+          user.name
+        );
+        io.to(user.room).emit(
+          "newMessage",
+          generateMessage(ADMIN_ROLE, `${user.name} ${USER_LEAVE_THE_CHAT}`)
+        );
+      }
+    });
+  });
+}
+
+module.exports = { chatHandler };
