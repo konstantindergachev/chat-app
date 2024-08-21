31feaadebbb595de3736fe30a660f4c754b2ef68
Kostyantyn Dergachov <dergachevkonstantin@gmail.com>
2024-08-21 10:31:22 +0000

    Implemented the listener handlers

---
+++ public/js/handlers/index.js
@@ -0,0 +1,99 @@
+"use strict";
+
+import { UI_LANGUAGE } from "../constants/index.js";
+
+export const handleFormSubmit = async (ev, props) => {
+  ev.preventDefault();
+  const { fi, inputs, lang } = props;
+
+  inputs.forEach((input) => {
+    if (input.value === "") {
+      ev.preventDefault();
+
+      input.classList.add("error");
+      fi.validPlaceholder("placeholder", lang.value);
+    }
+    return input;
+  });
+
+  const [name, room] = [...ev.target];
+  const user = { name: name.value, room: room.value, lang: lang.value };
+  const response = await fetch("/", {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: { "Content-Type": "application/json" },
+  });
+  if (response.ok) location.href = response.url;
+};
+
+export const handleFocusIn = (ev) => {
+  const namePlaceholder =
+    lang.value === "en"
+      ? UI_LANGUAGE.en.NAME_PLACEHOLDER
+      : UI_LANGUAGE.ua.NAME_PLACEHOLDER;
+  const roomPlaceholder =
+    lang.value === "en"
+      ? UI_LANGUAGE.en.ROOM_PLACEHOLDER
+      : UI_LANGUAGE.ua.ROOM_PLACEHOLDER;
+  const msg = [namePlaceholder, roomPlaceholder];
+
+  const target = ev.target;
+  switch (target.getAttribute("name")) {
+    case "username": {
+      target.setAttribute("placeholder", msg[0]);
+      break;
+    }
+    case "room": {
+      target.setAttribute("placeholder", msg[1]);
+      break;
+    }
+  }
+  if (target.getAttribute("tabindex")) target.classList.remove("error");
+};
+
+export const handleFocusOut = (ev, props) => {
+  const { fi, lang } = props;
+  if (ev.target.value === "") {
+    ev.target.classList.add("error");
+    fi.validPlaceholder("placeholder", lang.value);
+  }
+};
+
+export const handleChange = (ev, props) => {
+  let { form, formHtml, formTemplate, footer, footerHtml, footerTemplate } =
+    props;
+
+  formHtml =
+    ev.target.value === "en"
+      ? Mustache.render(formTemplate, {
+          formTitle: UI_LANGUAGE.en.FORM_TITLE,
+          nameLabel: UI_LANGUAGE.en.NAME_LABEL,
+          namePlaceholder: UI_LANGUAGE.en.DEFAULT_NAME_PLACEHOLDER,
+          roomLabel: UI_LANGUAGE.en.ROOM_LABEL,
+          roomPlaceholder: UI_LANGUAGE.en.DEFAULT_ROOM_PLACEHOLDER,
+          formBtn: UI_LANGUAGE.en.FORM_BTN,
+        })
+      : Mustache.render(formTemplate, {
+          formTitle: UI_LANGUAGE.ua.FORM_TITLE,
+          nameLabel: UI_LANGUAGE.ua.NAME_LABEL,
+          namePlaceholder: UI_LANGUAGE.ua.DEFAULT_NAME_PLACEHOLDER,
+          roomLabel: UI_LANGUAGE.ua.ROOM_LABEL,
+          roomPlaceholder: UI_LANGUAGE.ua.DEFAULT_ROOM_PLACEHOLDER,
+          formBtn: UI_LANGUAGE.ua.FORM_BTN,
+        });
+  form.innerHTML = formHtml;
+
+  footerHtml =
+    ev.target.value === "en"
+      ? Mustache.render(footerTemplate, {
+          place: UI_LANGUAGE.en.PLACE,
+          author: UI_LANGUAGE.en.AUTHOR,
+          currentYear: `© ${new Date().getFullYear()}`,
+        })
+      : Mustache.render(footerTemplate, {
+          place: UI_LANGUAGE.ua.PLACE,
+          author: UI_LANGUAGE.ua.AUTHOR,
+          currentYear: `© ${new Date().getFullYear()}`,
+        });
+  footer.innerHTML = footerHtml;
+};
--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,13 @@
+"use strict";
+
 import { UI_LANGUAGE } from "./constants/index.js";
 import { Factory } from "./utils/validate.js";
-
-("use strict");
+import {
+  handleFormSubmit,
+  handleFocusIn,
+  handleFocusOut,
+  handleChange,
+} from "./handlers/index.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const lang = document.getElementById("lang");
@@ -10,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const footerTemplate = document.getElementById("footer__template").innerHTML;
   const footer = document.getElementById("footer");
 
-  let formHtml = Mustache.render(formTemplate, {
+  const formHtml = Mustache.render(formTemplate, {
     formTitle: UI_LANGUAGE.ua.FORM_TITLE,
     nameLabel: UI_LANGUAGE.ua.NAME_LABEL,
     namePlaceholder: UI_LANGUAGE.ua.DEFAULT_NAME_PLACEHOLDER,
@@ -20,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   form.innerHTML = formHtml;
 
-  let footerHtml = Mustache.render(footerTemplate, {
+  const footerHtml = Mustache.render(footerTemplate, {
     place: UI_LANGUAGE.ua.PLACE,
     author: UI_LANGUAGE.ua.AUTHOR,
     currentYear: `© ${new Date().getFullYear()}`,
@@ -29,92 +35,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const fi = Factory(".form__input");
   const inputs = document.querySelectorAll(".form__input");
-  form.addEventListener("submit", async (ev) => {
-    ev.preventDefault();
-
-    inputs.forEach((input) => {
-      if (input.value === "") {
-        ev.preventDefault();
-
-        input.classList.add("error");
-        fi.validPlaceholder("placeholder", lang.value);
-      }
-      return input;
-    });
-    const [name, room] = [...ev.target];
-    const user = { name: name.value, room: room.value, lang: lang.value };
-    const response = await fetch("/", {
-      method: "POST",
-      body: JSON.stringify(user),
-      headers: { "Content-Type": "application/json" },
-    });
-    if (response.ok) location.href = response.url;
-  });
-  form.addEventListener("focusin", (ev) => {
-    const namePlaceholder =
-      lang.value === "en"
-        ? UI_LANGUAGE.en.NAME_PLACEHOLDER
-        : UI_LANGUAGE.ua.NAME_PLACEHOLDER;
-    const roomPlaceholder =
-      lang.value === "en"
-        ? UI_LANGUAGE.en.ROOM_PLACEHOLDER
-        : UI_LANGUAGE.ua.ROOM_PLACEHOLDER;
-    const msg = [namePlaceholder, roomPlaceholder];
-
-    const target = ev.target;
-    switch (target.getAttribute("name")) {
-      case "username": {
-        target.setAttribute("placeholder", msg[0]);
-        break;
-      }
-      case "room": {
-        target.setAttribute("placeholder", msg[1]);
-        break;
-      }
-    }
-    if (target.getAttribute("tabindex")) target.classList.remove("error");
-  });
-
-  form.addEventListener("focusout", (ev) => {
-    if (ev.target.value === "") {
-      ev.target.classList.add("error");
-      fi.validPlaceholder("placeholder", lang.value);
-    }
-  });
-
-  lang.addEventListener("change", (ev) => {
-    formHtml =
-      ev.target.value === "en"
-        ? Mustache.render(formTemplate, {
-            formTitle: UI_LANGUAGE.en.FORM_TITLE,
-            nameLabel: UI_LANGUAGE.en.NAME_LABEL,
-            namePlaceholder: UI_LANGUAGE.en.DEFAULT_NAME_PLACEHOLDER,
-            roomLabel: UI_LANGUAGE.en.ROOM_LABEL,
-            roomPlaceholder: UI_LANGUAGE.en.DEFAULT_ROOM_PLACEHOLDER,
-            formBtn: UI_LANGUAGE.en.FORM_BTN,
-          })
-        : Mustache.render(formTemplate, {
-            formTitle: UI_LANGUAGE.ua.FORM_TITLE,
-            nameLabel: UI_LANGUAGE.ua.NAME_LABEL,
-            namePlaceholder: UI_LANGUAGE.ua.DEFAULT_NAME_PLACEHOLDER,
-            roomLabel: UI_LANGUAGE.ua.ROOM_LABEL,
-            roomPlaceholder: UI_LANGUAGE.ua.DEFAULT_ROOM_PLACEHOLDER,
-            formBtn: UI_LANGUAGE.ua.FORM_BTN,
-          });
-    form.innerHTML = formHtml;
-
-    footerHtml =
-      ev.target.value === "en"
-        ? Mustache.render(footerTemplate, {
-            place: UI_LANGUAGE.en.PLACE,
-            author: UI_LANGUAGE.en.AUTHOR,
-            currentYear: `© ${new Date().getFullYear()}`,
-          })
-        : Mustache.render(footerTemplate, {
-            place: UI_LANGUAGE.ua.PLACE,
-            author: UI_LANGUAGE.ua.AUTHOR,
-            currentYear: `© ${new Date().getFullYear()}`,
-          });
-    footer.innerHTML = footerHtml;
-  });
+  form.addEventListener("submit", (ev) =>
+    handleFormSubmit(ev, { fi, inputs, lang })
+  );
+  form.addEventListener("focusin", handleFocusIn);
+  form.addEventListener("focusout", (ev) => handleFocusOut(ev, { fi, lang }));
+  lang.addEventListener("change", (ev) =>
+    handleChange(ev, {
+      form,
+      formHtml,
+      formTemplate,
+      footer,
+      footerHtml,
+      footerTemplate,
+    })
+  );
 });
